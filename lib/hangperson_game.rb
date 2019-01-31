bf873d21983ae0ad7716c572f68e1c991a230aa7
Tim <g00333333@gmit.ie>
2019-01-31 17:46:49 +0000

    Ready for Heroku

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,15 +1,73 @@
 class HangpersonGame
-
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
-
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
+  attr_accessor :word, :guesses, :wrong_guesses
+  
+  def guess c
+    # if it is in the alphabet
+    if c =~ /[[:alpha:]]/
+      # downcase the input
+      c.downcase!
+      
+      # if the word contains c or not
+      if @word.include? c
+        # if the same correct guess has not been made
+        if !@guesses.include? c
+          @guesses.concat c
+        else
+          return false
+        end
+      else
+        # if the same wrong guess has not been made
+        if !@wrong_guesses.include? c
+          @wrong_guesses.concat c
+        else
+          return false
+        end
+      end
+    else 
+      # character is invalid
+      raise ArgumentError
+    end
+  end
+  
+  def word_with_guesses
+    result = ""
+    
+    # for each letter in word
+    @word.each_char do |letter| 
+     # if the letter is contained within @guesses
+     if @guesses.include? letter
+       result.concat letter
+     else
+       result.concat '-'
+     end
+    end
+    
+    return result
+  end
+  
+  def check_win_or_lose
+    # if guessed wrong 7 or more times
+    return :lose if @wrong_guesses.length >= 7
+    # count the number of letters matched between @guesses and @word
+    counter = 0
+    word.each_char do |letter|
+      if @guesses.include? letter
+        counter += 1
+      end
+    end
+    
+    # if each letter has been accounted for
+    return :win if @word.length == counter
+    # keep playing
+    return :play
+  end
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
