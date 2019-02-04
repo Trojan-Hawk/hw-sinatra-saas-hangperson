f0e2172d27b9235a6c635c51b48f46ce5ef710cd
Tim <g00333333@gmit.ie>
2019-02-04 17:03:15 +0000

    completed win and lose conditions, prevented URL tampering

--- app.rb
+++ app.rb
@@ -39,7 +39,11 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    begin
+      flash[:message] = "You have already used that letter." if !@game.guess letter
+    rescue
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -49,18 +53,30 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose
+      when :win then redirect '/win'
+      when :lose then redirect '/lose'
+    else
+      erb :show
+    end
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    case @game.check_win_or_lose
+      when :win then erb :win
+    else
+      flash[:message] = "Nice try ;)."
+      erb :show
+    end
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    case @game.check_win_or_lose
+      when :lose then erb :lose
+    else
+      flash[:message] = "Giving up too easily.. Try again!"
+      erb :show
+    end
   end
   
 end
